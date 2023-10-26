ae7a32b229c0469687e57da654b5f43834fa3a50
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-10-26 00:37:04 -0700

    add a new route handler for /urls

--- express_server.js
+++ express_server.js
@@ -21,6 +21,16 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars); // res.render is used to load up an ejs view file
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
