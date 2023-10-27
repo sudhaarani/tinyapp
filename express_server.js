d806ed70742b0c5b7a080f5978ab53c47476f15e
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-10-27 11:44:11 -0700

    to store short-url and long-url key value pair to database when a POST request is received to /urls and to respond with a redirect from /urls to /urls/:id and add /u/:idrouting middleware to redirect to longURL(google.com) from db using shortURL as key

--- express_server.js
+++ express_server.js
@@ -14,14 +14,16 @@ app.use(express.urlencoded({ extended: true })); //When our browser submits a PO
 //middleware(this line, express library gives this method) which will translate
 
 function generateRandomString() {
-  const v = Math.random().toString(36).substring(2, 8);
+  shortUrlId = Math.random().toString(36).substring(2, 8);
+  console.log("shortUrlId::", shortUrlId);
+  return shortUrlId;
 }
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { //routing middleware
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
+app.get("/urls.json", (req, res) => {  //routing middleware
   res.json(urlDatabase);
 });
 
@@ -39,13 +41,21 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  console.log("input field::", req.body.longURL); // Log the POST request body to the console
+  const id = generateRandomString();
+  urlDatabase[id] = req.body.longURL
+  console.log("urlDatabase::", urlDatabase);
+  res.redirect("/urls/" + id); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
