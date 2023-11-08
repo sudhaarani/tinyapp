473be65a37e1896a2874f8cd7f9cd1df8127e01d
dhaaranisuriya2605@gmail.com <dhaaranisuriya2605@gmail.com>
2023-11-08 11:42:59 -0800

    added database objects

---
+++ database.js
@@ -0,0 +1,15 @@
+
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+let users = {};
+
+module.exports = { urlDatabase, users };
